ade032e12fa6e33ba2553056475a377cf605b63d
Luigi Salcedo <csalcedoe@unicartagena.edu.co>
2024-03-24 13:22:14 -0500

    project initialization

---
+++ src/main/java/com/inmobicasaventas/gestinmo/GestinmoApplication.java
@@ -0,0 +1,13 @@
+package com.inmobicasaventas.gestinmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestinmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestinmoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inmobicasaventas/gestinmo/GestinmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmobicasaventas.gestinmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestinmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
