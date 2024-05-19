47a1b8730ae06110735987c5825a1578dd60fab2
Luigi Salcedo <csalcedoe@unicartagena.edu.co>
2024-05-19 13:58:44 -0500

    added(property/infrastructure): se agregaron los endpoints para el acceso a los servicios

--- src/main/java/com/inmobicasaventas/gestinmo/api/properties/infrastructure/controllers/PropertyController.java
+++ src/main/java/com/inmobicasaventas/gestinmo/api/properties/infrastructure/controllers/PropertyController.java
@@ -1,7 +1,11 @@
 package com.inmobicasaventas.gestinmo.api.properties.infrastructure.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.inmobicasaventas.gestinmo.api.properties.application.services.PropertyService;
 import com.inmobicasaventas.gestinmo.api.properties.infrastructure.mappers.PropertyMapper;
 import com.inmobicasaventas.gestinmo.api.properties.infrastructure.mappers.dto.SavePropertyDto;
+import com.inmobicasaventas.gestinmo.api.properties.infrastructure.mappers.dto.SearchPropertyDto;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -25,9 +30,46 @@ public class PropertyController {
     private PropertyMapper propertyMapper;
 
     @PostMapping("save")
-    public ResponseEntity saveProperty(@Valid @RequestBody SavePropertyDto savePropertyDto) {
+    public ResponseEntity<Object> saveProperty(@Valid @RequestBody SavePropertyDto savePropertyDto) {
         var property = propertyMapper.toProperty(savePropertyDto);
         propertyService.saveProperty(property);
         return ResponseEntity.created(null).build();
     }
+
+    @GetMapping("search/client/{clientId}")
+    public ResponseEntity<List<SearchPropertyDto>> searchByClientId(@PathVariable String clientId) {
+        return ResponseEntity.ok(
+            propertyMapper.toSearchPropertyDtoList(
+                propertyService.searchPropertyByClient(clientId)
+        ));
+    }
+
+    @GetMapping("search/id/{id}")
+    public ResponseEntity<SearchPropertyDto> searchById(@PathVariable Integer id) {
+        return ResponseEntity.ok(
+            propertyMapper.toSearchPropertyDto(
+                propertyService.searchPropertyById(id)
+            )
+        );
+    }
+
+    @GetMapping("search/neighborhood/{neigborhoodId}")
+    public ResponseEntity<List<SearchPropertyDto>> searchByNeighborhood(@PathVariable Integer negihborhoodId) {
+        return ResponseEntity.ok(
+            propertyMapper.toSearchPropertyDtoList(
+                propertyService.searchPropertyByNeihborhood(negihborhoodId)
+            )
+        );
+    }
+
+    @GetMapping("search/type/{typeId}")
+    public ResponseEntity<List<SearchPropertyDto>> searchByType(@PathVariable Integer typeId) {
+        return ResponseEntity.ok(
+            propertyMapper.toSearchPropertyDtoList(
+                propertyService.searchPropertyByType(typeId)
+            )
+        );
+    }
+    
+    
 }
